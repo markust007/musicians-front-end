de728b98177220b2ac263b4e5cbda3f715a60bf5
Mark Carey <markust007@hotmail.com>
2020-10-29 16:38:10 -0500

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             "/": {
-                target: "http://localhost:3001"
+                // target: "http://localhost:3001"
+                target: 'https://musicians-back-end.herokuapp.com'
             }
         }
     }
